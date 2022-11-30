32ae225a3aa2c674e84841045ae8c68370c59d6d
Sofia22012005 <114824939+Sofia22012005@users.noreply.github.com>
2022-12-01 00:11:38 +0200

    Add files via upload

---
+++ laba6.2(1)/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba6.2(1)/laba6.2(1).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(MaxMin)
+		{
+			const int n = 10;
+			int a[n]{ -79,43,-78,-4,8,-67,45,-21,97,-42 };
+			int max = 97;
+			int min = -79;
+			Assert::AreEqual(max, min);
+
+		}
+	};
+}
